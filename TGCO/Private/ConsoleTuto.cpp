a416b3ceca474045a3d6e499e479a429621ccec9
bcronertrevenge <zbrunoz@hotmail.fr>
2014-12-31 12:25:27 +0100

    Adding AConsoleTuto which implements InteractiveElement. It contains all its function plus a variable bUnlocked with its getter/setter. All these functions can be called in Blueprint. It's also possible to start an event of one of these function in a Blueprint class whose parent is AConsoleTuto. Checklist : 	- Functions overrided 	- Implements correctly the interface 	- Callable in Blueprint 	- Comment on every functions and variables

---
+++ TGCO/Private/ConsoleTuto.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TGCO.h"
+#include "ConsoleTuto.h"
+
+AConsoleTuto::AConsoleTuto(const class FPostConstructInitializeProperties& PCIP) : Super(PCIP)
+{
+
+}
+
+bool AConsoleTuto::IsUnlocked()
+{
+	return bIsUnlocked;
+}
+
+void AConsoleTuto::SetUnlocked(bool _unlock)
+{
+	bIsUnlocked = _unlock;
+}
+
+void AConsoleTuto::OnInteract_Implementation()
+{
+		//TODO
+}
+
+void AConsoleTuto::Highlight_Implementation(bool highlight)
+{
+	//TODO
+}
+
+bool AConsoleTuto::IsInteractive_Implementation()
+{
+	return bIsInteractive;
+}
+
+void AConsoleTuto::OnLookAt_Implementation()
+{
+	//TODO
+}
+
+void AConsoleTuto::SetInteractive_Implementation(bool _interactive)
+{
+	bIsInteractive = _interactive;
+}
+
+void AConsoleTuto::OnComponentBeginOverlap_Implementation()
+{
+	Highlight_Implementation(true);
+}
+
+void AConsoleTuto::OnComponentEndOverlap_Implementation()
+{
+	Highlight_Implementation(false);
+}
