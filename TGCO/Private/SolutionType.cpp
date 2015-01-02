090f68f33c54d0518fa9ce04ed3a35dc0ffbf920
MarineDos <marinedos@gmail.com>
2015-01-02 19:15:49 +0100

    Create SolutionType enumeration
    Checklist
    	* Create SolutionType.h
    	* Create SolutionType.cpp
    	* Add values in SolutionType
    	* Create GetNameOfTheSolution function to get a string of the SolutionType

---
+++ TGCO/Private/SolutionType.cpp
@@ -0,0 +1,24 @@
+
+#include "TGCO.h"
+#include "SolutionType.h"
+
+FString GetNameOfTheSolution(const ESolutionType::Type SolutionType)
+{
+	switch(SolutionType)
+	{
+		case ESolutionType::ACID:					return TEXT("Acid");
+		case ESolutionType::CAUSTIC_SODA:			return TEXT("Caustic acid");
+		case ESolutionType::SALTED_WATER:			return TEXT("Salted Water");
+		case ESolutionType::ETHANOL:				return TEXT("Ethanol");
+		case ESolutionType::MERCURY:				return TEXT("Mercury");
+		case ESolutionType::WEEDKILLING:			return TEXT("Weedkilling");
+		case ESolutionType::INFLAMMABLE_PRODUCT:	return TEXT("Inflammable product");
+		case ESolutionType::FROZEN_PRODUCT:			return TEXT("Frozen product");
+		case ESolutionType::NITROGLYCERINE:			return TEXT("Nitroglycerine");
+		case ESolutionType::RIGIDIFY:				return TEXT("Rigidify");
+		default:									return TEXT("Unknown");
+	}
+
+}
+
+
