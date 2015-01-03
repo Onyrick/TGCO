c0e3f808bd5c79515857dcb0d522ff4a148627e7
bcronertrevenge <zbrunoz@hotmail.fr>
2015-01-03 13:10:23 +0100

    Add Class EnergyCell Adding the Actor EnergyCell which implements Trigerrable. Its functions and its variables have been added and are callable in BP. Checklist : 	- Add new class 	- Add its functions 	- Add its variable 	- Visible in BP 	- Works Correctly

--- TGCO/Public/InteractiveElement/ConsoleTuto.h
+++ TGCO/Public/InteractiveElement/ConsoleTuto.h
@@ -19,51 +19,54 @@ public:
 	//Method called when the player wants to use the object which is currently hightlighted.
 	//This method will be overriden by each of the class' children
 	//////////////////////////////////////////////////////////////////////////////////////////////
-	UFUNCTION(BlueprintCallable, Category = "InteractiveElement Console", meta = (FriendlyName = "On Interact"))
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "On Interact"))
 		virtual void OnInteract();
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	//Method used to highlight the interactive element that the player is currently looking at.
 	//////////////////////////////////////////////////////////////////////////////////////////////
-	UFUNCTION(BlueprintCallable, Category = "InteractiveElement Console", meta = (FriendlyName = "Highlight"))
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "Highlight"))
 		virtual void Highlight(bool _highlight);
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	//Check whether or not the targeted element is currently active(eg the flower pot in the past 
 	//	is not interactive until the player grabs the seed)
 	//////////////////////////////////////////////////////////////////////////////////////////////
-	UFUNCTION(BlueprintCallable, Category = "InteractiveElement  Console", meta = (FriendlyName = "IsInteractive"))
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "IsInteractive"))
 		virtual bool IsInteractive();
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	//for an element to be hightlighted it needs the player to look at it and that the player is close
 	//	enough to use it.To know if the player is looking at the element it listens to the evenement OnLookAt
 	//////////////////////////////////////////////////////////////////////////////////////////////
-	UFUNCTION(BlueprintCallable, Category = "InteractiveElement  Console", meta = (FriendlyName = "OnLookAt"))
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "OnLookAt"))
 		virtual void OnLookAt();
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	//Set the element as being interactible
 	//////////////////////////////////////////////////////////////////////////////////////////////
-	UFUNCTION(BlueprintCallable, Category = "InteractiveElement Console", meta = (FriendlyName = "SetInteractive"))
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "SetInteractive"))
 		virtual void SetInteractive(bool _interactive);
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	//for an element to be hightlighted it needs the player to look at it and that the player is close enough
 	// to use it.To know if the player is close enough it listens to the evenement OnComponentBeginOverlap
 	//////////////////////////////////////////////////////////////////////////////////////////////
-	UFUNCTION(BlueprintCallable, Category = "InteractiveElement Console", meta = (FriendlyName = "OnComponentBeginOverlap"))
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "OnComponentBeginOverlap"))
 		virtual void OnComponentBeginOverlap();
 
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	//When the player leaves the area where he can use an object, this interactive element receives
 	//	OnComponentEndOverlap so that it can stop the highlighting
 	//////////////////////////////////////////////////////////////////////////////////////////////
-	UFUNCTION(BlueprintCallable, Category = "InteractiveElement Console", meta = (FriendlyName = "OnComponentEndOverlap"))
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "OnComponentEndOverlap"))
 		virtual void OnComponentEndOverlap();
 	
 	//Getter / Setter / Is
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "IsUnlocked"))
 		bool IsUnlocked();
+
+	UFUNCTION(BlueprintCallable, Category = "Console Tuto", meta = (FriendlyName = "SetUnlocked"))
 		void SetUnlocked(bool _unlock);
 
 protected:
