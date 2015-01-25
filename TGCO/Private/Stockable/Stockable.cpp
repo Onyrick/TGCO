e8b048b7e5ab9f842c5edf1f27abec3a38262e97
bcronertrevenge <bcronert@etud.u-pem.fr>
2015-01-25 13:39:52 +0100

    Add Stockable Classes

---
+++ TGCO/Private/Stockable/Stockable.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TGCO.h"
+#include "TGCOCharacter.h"
+#include "Stockable.h"
+
+AStockable::AStockable(const class FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer),
+sDescription("Stockable")
+{
+
+}
+
+void AStockable::OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogDebug, Warning, TEXT("Begin Overlap Stockable"));
+	Destroy();
+}
+
+void AStockable::OnOverlapEnd(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogDebug, Warning, TEXT("End Overlap Stockable"));
+}
+
+FString AStockable::GetDescription()
+{
+	return sDescription;
+}
