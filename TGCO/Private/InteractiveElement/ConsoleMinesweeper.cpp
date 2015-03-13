c0b8eaecc5cb4558ab543b937f89d260a1c244da
Lucie2lr <lucie2lr@gmail.com>
2015-03-13 11:58:29 +0100

    Change the mode when activate the console

--- TGCO/Private/InteractiveElement/ConsoleMinesweeper.cpp
+++ TGCO/Private/InteractiveElement/ConsoleMinesweeper.cpp
@@ -2,6 +2,7 @@
 
 #include "TGCO.h"
 #include "TGCOPlayerController.h"
+#include "TGCOPlayerState.h"
 #include "ConsoleMinesweeper.h"
 
 
@@ -25,6 +26,13 @@ bool AConsoleMinesweeper::OnInteract()
 				UE_LOG(LogTest, Warning, TEXT("Console activate"));
 				PlayerController->SetViewTargetWithBlend(CameraConsole, 1.5, EViewTargetBlendFunction::VTBlend_EaseInOut, 1.0, true);
 				EnableInput(PlayerController);
+				FInputModeGameAndUI  Mode;
+				PlayerController->SetInputMode(Mode);
+				ATGCOPlayerState* PlayerState = Cast<ATGCOPlayerState>(PlayerController->PlayerState);
+				/*if (PlayerState)
+				{
+					PlayerState->EnterInAPuzzle();
+				}*/
 				PlayerController->SetIgnoreMoveInput(true);
 				PlayerController->SetIgnoreLookInput(true);
 				PlayerController->bShowMouseCursor = true;
@@ -38,6 +46,13 @@ bool AConsoleMinesweeper::OnInteract()
 				ACharacter* PlayerCharacter = PlayerController->GetCharacter();
 				PlayerController->SetViewTargetWithBlend(PlayerCharacter, 1.5, EViewTargetBlendFunction::VTBlend_EaseInOut, 1.0, true);
 				DisableInput(PlayerController);
+				FInputModeGameOnly GameMode;
+				ATGCOPlayerState* PlayerState = Cast<ATGCOPlayerState>(PlayerController->PlayerState);
+				/*if (PlayerState)
+				{
+					PlayerState->LeaveAPuzzle();
+				}*/
+				PlayerController->SetInputMode(GameMode);
 				PlayerController->SetIgnoreMoveInput(false);
 				PlayerController->SetIgnoreLookInput(false);
 				PlayerController->bShowMouseCursor = false;
@@ -54,13 +69,4 @@ bool AConsoleMinesweeper::OnInteract()
 void AConsoleMinesweeper::ResetMinesweeper_Implementation()
 {
 	UE_LOG(LogTest, Warning, TEXT("Je suis dans ResetMinesweeper"));
-	for (TActorIterator<AActor> ActorItr(GetWorld()); ActorItr; ++ActorItr)
-	{
-		if (ActorItr->GetName().Contains("Minesweeper_Futur") && ActorItr->GetActorClass()->GetDescription() == FString(TEXT("Minesweeper BP")))
-		{
-			AMinesweeper* Minesweeper = Cast<AMinesweeper>(*ActorItr);
-			Minesweeper->GetMineBoxAt(0)->SetVisibilityOfFlag();
-		}
-	}
 }
-
